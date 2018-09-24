18349a87736a3070226994a24203f2aa423c0725
Jordy Van der Haegen <jordyvanderhaegen@Jordys-MacBook-Pro.local>
2018-09-24 21:06:08 +0200

    nukeee

--- Code/GreewardRedux/src/components/common/index.js
+++
@@ -1,7 +0,0 @@
-export * from './Button';
-export * from './Header';
-export * from './Item';
-export * from './Container';
-export * from './Input';
-export * from './Spinner';
-export * from './Confirm';
